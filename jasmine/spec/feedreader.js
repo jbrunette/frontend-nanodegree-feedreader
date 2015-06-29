8e2f4dbd2b017149f1ed8456b841c0bb0b9e9320
jbrunette <jason.brunette@gmail.com>
2015-06-29 14:50:49 -0500

    updates per reviewer comments

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,33 +27,33 @@ $(function() {
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         for (var i=0; i<allFeeds.length; i++) {
+         for (var i = 0; i < allFeeds.length; i++) {
             test_allFeeds_item_for_url(allFeeds[i]);
          }
          function test_allFeeds_item_for_url(item) {
             it('has a URL defined and populated', function() {
                 expect(item.url).toBeDefined();
                 expect(item.url).not.toBe("");
-            })
+            });
          }
 
 
-        /* TODO: Write a test that loops through each feed
+        /* Test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-        for (var i=0; i<allFeeds.length; i++) {
+        for (var i = 0; i < allFeeds.length; i++) {
             test_allFeeds_item_for_name(allFeeds[i]);
          }
          function test_allFeeds_item_for_name(item) {
             it('has a name defined and populated', function() {
                 expect(item.name).toBeDefined();
                 expect(item.name).not.toBe("");
-            })
+            });
          }
     });
 
@@ -77,57 +77,53 @@ $(function() {
             // Click menu icon again and verify that menu is hidden
             $("i.icon-list").click();
             expect($("body").hasClass("menu-hidden")).toBe(hidden);
-        })
-    })
+        });
+    });
 
     // Test that feed loads entries successfully
     describe('Initial entries', function() {
 
-      // Run loadFeed before each test, ensuring that we can run the test once the async call returns
-      beforeEach(function(done) {
-        loadFeed(0, function() {
-            done();
+        // Run loadFeed before each test, ensuring that we can run the test once the async call returns
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
         });
-      });
 
-      // Test if .entry elements are added to DOM when feed is loaded
-      it('successfully loads feed data', function(done) {
-        expect($(".feed .entry").length).not.toBe(0);
-        done();
-      });
+        // Test if .entry elements are added to DOM when feed is loaded
+        it('successfully loads feed data', function() {
+            expect($(".feed .entry").length).not.toBe(0);
+        });
     });
 
     // Test that a new feed selection loads entries and updates the elements successfully
     describe('New feed selection', function() {
-      var first_feed_item_link = "";
+        var first_feed_item_link = "";
 
-      // Perform actions before test
-      beforeEach(function(done) {
+        // Perform actions before test
+        beforeEach(function(done) {
 
-        // Load first feed
-        loadFeed(0, function() {
+            // Load first feed
+            loadFeed(0, function() {
 
-          // Get the link for the first item from the initial feed
-          var first_feed_item = $(".feed .entry-link")[0];
-          if (first_feed_item) {
-            first_feed_item_link = first_feed_item.href;
-          }
+                // Get the link for the first item from the initial feed
+                var first_feed_item = $(".feed .entry-link")[0];
+                if (first_feed_item) {
+                    first_feed_item_link = first_feed_item.href;
+                }
+
+                // Load second feed
+                loadFeed(1, function() {
+
+                    // Ready to run test
+                    done();
+                });
+            });
         });
 
-        // Load second feed
-        loadFeed(1, function() {
-
-          // Ready to run test
-          done();
+        // Test to see if first link in item list is different now after second feed is loaded
+        it('successfully switches feeds', function() {
+            expect($(".feed .entry-link")[0].href).not.toBe(first_feed_item_link);
         });
-
-      });
-
-      // Test to see if first link in item list is different now after second feed is loaded
-      it('successfully switches feeds', function(done) {
-        expect($(".feed .entry-link")[0].href).not.toBe(first_feed_item_link);
-        done();
-      });
     });
-
 }());
